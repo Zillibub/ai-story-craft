fdd5454dcf126a3275df3cf22ee986232f75bfb3
Serj <serj2work@gmail.com>
2024-07-20 13:57:43 +0100

    Moved creation to a class method

--- ai_story_craft/assistant.py
+++ ai_story_craft/assistant.py
@@ -1,36 +1,45 @@
 import openai
 from pathlib import Path
+from dataclasses import dataclass
 from core.settings import settings
 
 
-class AssistantManager:
-    def __init(self):
+class Assistant:
+    def __init__(self, assistant_id: str):
+        self.assistant_id = assistant_id
         self.client = openai.Client(api_key=settings.openai_api_key)
 
+    @classmethod
     def create_assistant(
-            self,
+            cls,
             name: str,
             subtitle_file: Path,
     ):
+        """
+        Create an assistant with a file search tool from a given subtitle path file.
+        :param name:
+        :param subtitle_file:
+        :return:
+        """
         if not subtitle_file.exists():
             raise FileNotFoundError(f"Subtitle file not found: {subtitle_file}")
 
-        vector_store = self.client.beta.vector_stores.create(name=subtitle_file.stem)
+        client = openai.Client(api_key=settings.openai_api_key)
+        vector_store = client.beta.vector_stores.create(name=subtitle_file.stem)
 
-        # Use the upload and poll SDK helper to upload the files, add them to the vector store,
-        # and poll the status of the file batch for completion.
-        file_batch = self.client.beta.vector_stores.file_batches.upload_and_poll(
+        file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
             vector_store_id=vector_store.id, files=[open(subtitle_file, "rb")]
         )
         print(file_batch.status)
         print(file_batch.file_counts)
 
-        assistant = self.client.beta.assistants.create(
+        assistant = client.beta.assistants.create(
             model=settings.assistant_model,
-            description=f"Assistant for {name}",
+            name=name,
             instructions="""You are a senior product manager who creates a user story for a new feature.
             Create the user story based only on provided information without adding any additional details. 
             """,
             tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
         )
 
+        return cls(assistant['id'])
