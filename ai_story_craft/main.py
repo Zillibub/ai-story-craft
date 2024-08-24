109cbb203d567e5b2062451f686aa7914b12bc43
Serge Kozloff <serj2work@gmail.com>
2024-08-24 11:06:31 +0100

    Deleted useless file

--- ai_story_craft/main.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-from assistant import create_assistant
-from subtitles_extractor import extract_subtitles
-
-
-if __name__ == "__main__":
-    assistant = create_assistant(name='assistant_1', subtitle_file=Path('../../data/short/subtitles.txt'))
-
-    pass
