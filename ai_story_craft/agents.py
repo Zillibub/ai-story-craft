4de9b66c1c345e3ea13233c5b3c14f25846e0b2c
Serge Kozloff <serj2work@gmail.com>
2024-10-18 15:47:09 +0100

    Added user story mapping prompt

--- ai_story_craft/agents.py
+++ ai_story_craft/agents.py
@@ -17,29 +17,39 @@ class ProductManager:
     """
 
     get_image_timestamp: str = """You are a senior product manager who analyses the product videos.
-    You are provided with a set of video subtitles. and you need to find the timestamp of the image in the 
-    video based on the provided description.
-    Use only the provided information to answer the user's questions.\n
-    Return only the timestamp of the image in the video as one number. \n
-    Description: {description} \nAnswer:
+        You are provided with a set of video subtitles. and you need to find the timestamp of the image in the 
+        video based on the provided description.
+        Use only the provided information to answer the user's questions.\n
+        Return only the timestamp of the image in the video as one number. \n
+        Description: {description} \nAnswer:
     """
 
     get_image_name: str = """You are provided with a description for a requested image. 
-    Return proper image name. It should ber shorter than 100 symbols, no spaced or special characters and without 
-    extension. Return only the file name. \n
-    Description: {description} \nAnswer:
+        Return proper image name. It should ber shorter than 100 symbols, no spaced or special characters and without 
+        extension. Return only the file name. \n
+        Description: {description} \nAnswer:
     """
 
     assistant_description_prompt: str = """
-    Create an assistant description based on provided documentation. It should be no more than 400 symbols.
-    it should clearly describe with content of the documentation. Do not add any links to the sources. 
+        Create an assistant description based on provided documentation. It should be no more than 400 symbols.
+        it should clearly describe with content of the documentation. Do not add any links to the sources. 
+    """
+
+    user_story_mapping: str = """
+        You are a senior product manager who analyses the product videos. 
+        You will be provided with a video subtitles with product description. 
+        Create a user story map for the presented product. 
+        Each task should have the folloing information: 
+        1. Name 2. Description 3. Goal 4. Reason 
+        
+        Return only user story map. Use only provided information. 
     """
 
     telegram_formatting: str = """
-    You are given a text. Format it to your best judgement based on these rules: 
-    <b>text</b> - Bold text, <i>text</i> - Italicize text, <u>text</u> - Underline text, 
-    <s>text</s> - Strikethrough text, <code>text</code> - highlight part of a piece of code, 
-    <tg-spoiler>text</tg-spoiler> - spoiler formatting that hides the selected text, 
-    <a href="http://www.example.com/">text</a>	Creates a hyperlink to the selected text. 
-    Do not change text content. 
+        You are given a text. Format it to your best judgement based on these rules: 
+        <b>text</b> - Bold text, <i>text</i> - Italicize text, <u>text</u> - Underline text, 
+        <s>text</s> - Strikethrough text, <code>text</code> - highlight part of a piece of code, 
+        <tg-spoiler>text</tg-spoiler> - spoiler formatting that hides the selected text, 
+        <a href="http://www.example.com/">text</a>	Creates a hyperlink to the selected text. 
+        Do not change text content. 
     """
